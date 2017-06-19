c9bc9106e14cfb892e1f5950804675af049031fa
Lucien <1155064923@link.cuhk.edu.hk>
2017-06-19 10:39:32 -0400

    update

--- fast_run.sh
+++ fast_run.sh
@@ -18,42 +18,42 @@ python genInfos.py --n_subjects=$n_subjects
 # find $workdir/lvl1/report/ -type f -name '*.csv' -print0 | xargs -0 rm --
 # rm -rf $workdir/lvl1/report/*
 
-cd $workdir/lvl1
-for i in "${array[@]}"
-do
-    # Low pass EEG model x 2
-    $keras_python genPreds.py models/FBL.yml $i --n_subjects=$n_subjects &
+# cd $workdir/lvl1
+# for i in "${array[@]}"
+# do
+#     # Low pass EEG model x 2
+#     $keras_python genPreds.py models/FBL.yml $i --n_subjects=$n_subjects &
+# 
+#     # NN models
+#     $keras_python genPreds_RNN.py models/NN_16.yml $i --n_subjects=$n_subjects
+#     $keras_python genPreds_RNN.py models/NN_32.yml $i --n_subjects=$n_subjects
+#     $keras_python genPreds_RNN.py models/NN_64.yml $i --n_subjects=$n_subjects
+#     $keras_python genPreds_RNN.py models/NN_128.yml $i --n_subjects=$n_subjects
+#     $keras_python genPreds_RNN.py models/NN_256.yml $i --n_subjects=$n_subjects
+#     $keras_python genPreds_RNN.py models/NN_512.yml $i --n_subjects=$n_subjects
+#     wait
+# done
 
-    # NN models
-    $keras_python genPreds_RNN.py models/NN_16.yml val --n_subjects=$n_subjects
-    $keras_python genPreds_RNN.py models/NN_32.yml val --n_subjects=$n_subjects
-    $keras_python genPreds_RNN.py models/NN_64.yml val --n_subjects=$n_subjects
-    $keras_python genPreds_RNN.py models/NN_128.yml $i --n_subjects=$n_subjects
-    $keras_python genPreds_RNN.py models/NN_256.yml $i --n_subjects=$n_subjects
-    $keras_python genPreds_RNN.py models/NN_512.yml $i --n_subjects=$n_subjects
-    wait
-done
-
-cd $workdir/lvl2
-for i in "${array[@]}"
-do
-  for filename in models/xgb_NN_FBL*.yml; do
-    echo "$filename"
-
-    if [[ "$filename" == *"bags_model"* ]]
-    then
-      $keras_python genEns_BagsModels.py $filename $i --n_subjects=$n_subjects -fast 1
-      # echo "pass genEns_BagsModels.py"
-    elif [[ "$filename" == *"bags"* ]]
-    then
-      $keras_python genEns_BagsSubjects.py $filename $i --n_subjects=$n_subjects -fast 1
-      # echo "pass genEns_BagsSubjects.py"
-    else
-      $keras_python genEns.py $filename $i --n_subjects=$n_subjects -fast 1
-      # echo "pass genEns.py"
-    fi
-  done
-done
+# cd $workdir/lvl2
+# for i in "${array[@]}"
+# do
+#   for filename in models/xgb_NN_FBL*.yml; do
+#     echo "$filename"
+# 
+#     if [[ "$filename" == *"bags_model"* ]]
+#     then
+#       $keras_python genEns_BagsModels.py $filename $i --n_subjects=$n_subjects -fast 1
+#       # echo "pass genEns_BagsModels.py"
+#     elif [[ "$filename" == *"bags"* ]]
+#     then
+#       $keras_python genEns_BagsSubjects.py $filename $i --n_subjects=$n_subjects -fast 1
+#       # echo "pass genEns_BagsSubjects.py"
+#     else
+#       $keras_python genEns.py $filename $i --n_subjects=$n_subjects -fast 1
+#       # echo "pass genEns.py"
+#     fi
+#   done
+# done
 
 cd $workdir/lvl3
 $keras_python genFinal.py models/Fast.yml
